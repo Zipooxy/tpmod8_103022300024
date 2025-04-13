c852cdd90123b4da53709a63703b00808616938c
Zipooxy <142865402+Zipooxy@users.noreply.github.com>
2025-04-13 23:01:07 +0700

    menyelesaikan program dengan runtime configuration

--- tpmod8_103022300024/Program.cs
+++ tpmod8_103022300024/Program.cs
@@ -33,4 +33,48 @@ class CovidConfig
     }
 }
 
+class Program
+{
+    static void Main()
+    {
+        string configPath = "config.json";
+        CovidConfig config = CovidConfig.LoadConfig(configPath);
+
+        while (true)
+        {
+            Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.CONFIG1}");
+            double suhuBadan = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+            int hariGejala = int.Parse(Console.ReadLine());
+
+            bool suhuValid = config.CONFIG1 == "celcius"
+                ? suhuBadan >= 36.5 && suhuBadan <= 37.5
+                : suhuBadan >= 97.7 && suhuBadan <= 99.5;
+
+            if (suhuValid && hariGejala < config.CONFIG2)
+            {
+                Console.WriteLine(config.CONFIG4);
+            }
+            else
+            {
+                Console.WriteLine(config.CONFIG3);
+            }
+
+            Console.WriteLine("Apakah Anda ingin mengubah satuan suhu? (y/n)");
+            if (Console.ReadLine()?.ToLower() == "y")
+            {
+                config.UbahSatuan();
+                config.SaveConfig(configPath);
+                Console.WriteLine($"Satuan suhu telah diubah menjadi {config.CONFIG1}");
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
+
 
